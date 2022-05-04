0d3af78fd890897824d3bc60752d0fe074bceb9b
im524 <emii40574@gmail.com>
2022-05-04 12:17:47 +0000

    ajout api insertion

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -13,20 +13,42 @@ class PageController extends Controller
     }
 
     public function update(Request $request, $_id)
-   {
-        
-       $page = Page::find($_id);
+    {
 
-       $page->Titre = $request->get('Titre');
-       $page->Description = $request->get('Description');
-       $page->Rating = $request->get('Rating');
-       $page->Sommaire = $request->get('Sommaire');
-       $page->Thématique = $request->get('Thématique');
-       $page->Média = $request->get('Média');
-       $page->save();
+        $page = Page::find($_id);
 
-       return response()->json(["result" => "ok"], 201);
-   }
-    
+        $page->Titre = $request->get('Titre');
+        $page->Description = $request->get('Description');
+        $page->Rating = $request->get('Rating');
+        $page->Contenu = $request->get('Contenu');
+        $page->Thematique = $request->get('Thematique');
+        $page->Media = $request->get('Media');
+        $page->save();
 
+        return response()->json(["result" => "ok"], 201);
+    }
+
+    public function store(Request $request)
+    {
+        request()->validate([
+            'Titre' => 'required',
+            'Description' => 'required',
+            'Rating' => 'required',
+            'Contenu' => 'required',
+            'Thematique' => 'required',
+            'Media' => 'required',
+            'Createur' => 'required'
+        ]);
+        $page = new Page();
+        $page->Titre = $request->get('Titre');
+        $page->Description = $request->get('Description');
+        $page->Rating = $request->get('Rating');
+        $page->Contenu = $request->get('Contenu');
+        $page->Thematique = $request->get('Thematique');
+        $page->Media = $request->get('Media');
+        $page->Createur = $request->get('Createur');
+        $page->save();
+
+        return $page;
+    }
 }
