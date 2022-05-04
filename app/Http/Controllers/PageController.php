08b30c7c0d1e81fafaef9073a8ea0f103df1868d
y0ussra <youssraelhaddad.student@gmail.com>
2022-05-04 11:28:52 +0000

    Ajout de la partie modification page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -12,6 +12,21 @@ class PageController extends Controller
         return Page::all();
     }
 
+    public function update(Request $request, $_id)
+   {
+        
+       $page = Page::find($_id);
+
+       $page->Titre = $request->get('Titre');
+       $page->Description = $request->get('Description');
+       $page->Rating = $request->get('Rating');
+       $page->Sommaire = $request->get('Sommaire');
+       $page->Thématique = $request->get('Thématique');
+       $page->Média = $request->get('Média');
+       $page->save();
+
+       return response()->json(["result" => "ok"], 201);
+   }
     
 
 }
