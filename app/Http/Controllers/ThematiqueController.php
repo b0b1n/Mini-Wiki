8cf5659ca5a6e4f8ddb9185612c29ff1d613d0ce
b0b1n <sohaibskious@gmail.com>
2022-05-07 04:51:57 +0200

    ajout des searchs ett urls

--- app/Http/Controllers/ThematiqueController.php
+++ app/Http/Controllers/ThematiqueController.php
@@ -1,26 +1,33 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Thematique;
 use Illuminate\Http\Request;
 
 class ThematiqueController extends Controller
 {
+    public function them(Request $request)
+    {
+            $them = Thematique::where([
+            'NomThematique' => $request->them,
+        ])->get();
+
+        return $them;
+    }
+    public function themall(Request $request)
+    {
+        return Thematique::all();
+    }
     function search($name)
-        {
+    {
 
-            $thematiques = Thematique::where('NomThematique', 'LIKE', '%'. $name. '%')->get();
+        $thematiques = Thematique::where('NomThematique', 'LIKE', '%' . $name . '%')->get();
 
-            if(count($thematiques)){
-        return  Response()->json($thematiques);
-
-            }
-            else
-            {
+        if (count($thematiques)) {
+            return  Response()->json($thematiques);
+        } else {
             return response()->json(['Result' => 'No Data  found'], 404);
-            }
-
-
         }
+    }
 }
-
