6c81f405c736d74ec20ee4dcc84f3118c11a9ca0
funFan99 <bennourm237@gmail.com>
2022-05-08 17:02:02 +0100

    Laravel New Modification

--- app/Http/Controllers/RapportController.php
+++ app/Http/Controllers/RapportController.php
@@ -3,8 +3,63 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Rapport;
+use Illuminate\Support\Facades\Validator;
 
 class RapportController extends Controller
 {
-    //
-}
+    public function rapp(Request $request)
+    {
+          //$input = $request->all();
+     $validator =
+     Validator::make($request->all(),
+ [   
+     
+     'Utilisateur'=>'required|max:255',
+     'Page'=>'required|max:255',
+     'Contenu'=>'required|max:255',
+     
+ ]
+ );
+
+ //auth()->attempt($request->only('email','password'));
+ 
+ //return redirect()->route('dashbord');
+ 
+ if($validator->fails()){
+
+ return response()->json($validator->errors());
+     }else{
+         /*$rapp = Rapport::where('Utilisateur',$request->Utilisateur)
+         ->where('Page',$request->Page)
+         ->get();
+         //dd($users);
+         
+         //dd($users[0]->email);
+          if(empty($rapp)){*/
+            Rapport::create([
+          
+                 'Utilisateur' =>$request->Utilisateur,
+                 'Page'=>$request->Page,
+                 'Contenu' =>$request->Contenu
+                 
+                 
+                ]);
+                
+         }
+     }
+     public function rappt(Request $request)
+    {
+        $rapp = Rapport::where('Utilisateur',$request->Utilisateur)
+         ->where('Page',$request->Page)
+         ->get();
+
+        return $rapp;
+    }
+    public function repall(Request $request)
+    {
+        return Rapport::all();
+    }
+    
+    }
+
